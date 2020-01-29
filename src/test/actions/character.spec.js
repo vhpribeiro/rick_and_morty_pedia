ce9331d77fc287dcca2f6b6c17cb227a1bb7d3e1
Vitor Hugo Pereira Ribeiro <vitorhugopereiraribeiro@gmail.com>
2020-01-28 20:20:41 -0400

    Adding some test for character action

--- src/test/actions/character.spec.js
+++ src/test/actions/character.spec.js
@@ -1,25 +1,65 @@
-import { clearCharacter, searchCharacter } from '../../actions/character'
+import { clearCharacter, searchCharacter, fetchCharacter } from '../../actions/character'
 import { FETCH_CHARACTER, CLEAR_CHARACTER, SEARCH_CHARACTER } from '../../actions/types'
 
-test('should clear character search', () => {
-    const expectedResult = {
-        type: CLEAR_CHARACTER,
-        character: null
-    }
+import moxios from 'moxios'
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
 
-    const action = clearCharacter()
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
 
-    expect(action).toEqual(expectedResult)
-})
+describe('Character Action', () => {
+    beforeEach(() => {
+        moxios.install();
+    })
 
-test('should search for a character', () => {
-    const characterName = 'Rick Babyyy'
-    const expectedResult = {
-        type: SEARCH_CHARACTER,
-        searchCharacter: characterName
-    }
+    afterEach(() => {
+        moxios.uninstall();
+    })
 
-    const action = searchCharacter(characterName)
+    it('should clear character search', () => {
+        const expectedResult = {
+            type: CLEAR_CHARACTER,
+            character: null
+        }
+    
+        const action = clearCharacter()
+    
+        expect(action).toEqual(expectedResult)
+    })
+    
+    it('should search for a character', () => {
+        const characterName = 'Rick Babyyy'
+        const expectedResult = {
+            type: SEARCH_CHARACTER,
+            searchCharacter: characterName
+        }
+    
+        const action = searchCharacter(characterName)
+    
+        expect(action).toEqual(expectedResult)
+    })
 
-    expect(action).toEqual(expectedResult)
+    it('should fetch a character', async () => {
+        const characterId = 1
+        const data = {
+            id: characterId,
+            name: 'Rick Sanchez'
+        }
+        moxios.stubRequest(`https://rickandmortyapi.com/api/character/${characterId}`, {
+            status: 200,
+            response: data
+        })
+        const expectedActions = [{
+            type: FETCH_CHARACTER,
+            character: data
+        }]
+
+        const store = mockStore({})
+
+        await store.dispatch(fetchCharacter(characterId))
+            .then(() => {
+                expect(store.getActions()).toEqual(expectedActions)
+            })
+    })
 })
